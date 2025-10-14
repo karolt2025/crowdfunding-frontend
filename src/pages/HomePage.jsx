772172ad2c43b219f1123f1e567fdb2ca81fdb47
karoltravino <travino.karol@gmail.com>
2025-10-14 20:21:52 +0800

    first commit for crowdfunding frontend

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+
+import FundraiserCard from "../components/FundraiserCard";
+import useFundraisers from "../hooks/use-fundraisers.js";
+import "./HomePage.css";
+
+function HomePage() {
+    const {fundraisers} = useFundraisers();
+
+    return (
+        <div id="fundraiser-list">
+            {fundraisers.map((fundraiserData, key) => {
+                return <FundraiserCard key={key} fundraiserData={fundraiserData} />;
+            })}
+        </div>
+    );
+}
+
+export default HomePage;
