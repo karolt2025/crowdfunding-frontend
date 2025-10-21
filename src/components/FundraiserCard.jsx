f10f83cb354ec8abc8f85044940e1ce3e7d4df02
karoltravino <travino.karol@gmail.com>
2025-10-21 19:09:41 +0800

    removed the image

--- src/components/FundraiserCard.jsx
+++ src/components/FundraiserCard.jsx
@@ -7,7 +7,7 @@ function FundraiserCard(props) {
     return (
         <div className="fundraiser-card">
             <Link to="/fundraiser">
-                <img src={https://static.vecteezy.com/system/resources/thumbnails/051/043/331/small_2x/kids-drawing-illustration-collection-of-cute-children-s-drawings-of-kids-animals-nature-objects-hand-drawn-doodle-color-pencil-naive-sketch-children-cute-school-chalk-background-vector.jpg} />
+                <img src={fundraiserData.image} />
                 <h3>{fundraiserData.title}</h3>
             </Link>
         </div>
